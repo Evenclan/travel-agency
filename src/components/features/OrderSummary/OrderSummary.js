d8437bd453c40654168c4f4c34f0773bae2958da
Kamil Bogucki <boguckikamil@hotmail.com>
2020-05-24 14:09:54 +0200

    trip price problem (shows 0)

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,19 +1,30 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
+
+import PropTypes from 'prop-types';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+
+
+// import PropTypes from 'prop-types';
+
 
 class OrderSummary extends React.Component {
   static propTypes = {
-
+    tripCost: PropTypes.node,
+    options: PropTypes.any,
   }
 
   render() {
-
+    const {tripCost, options } = this.props;
     return (
-      <h2 className={styles.component}>Total: <strong>$12.345</strong></h2>
+      <h2 className={styles.component}>Total: <strong>{calculateTotal(formatPrice(tripCost), options)}</strong></h2>
     );
   }
 }
 
+
+
 export default OrderSummary;
+
 
