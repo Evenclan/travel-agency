77efb75aebf0c8d047410a1de50f11eda552bc8a
Kamil Bogucki <boguckikamil@hotmail.com>
2020-06-01 02:12:00 +0200

    fixed title test -> working on cost and days

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -12,13 +12,27 @@ it('should check if link to the adress match the ID', () => {
 });
 
 it ('should check src and alt of an IMG', () => {
-
   const expectedSrc = 'img.jpg';
   const expectedAlt = `imageAlt`;
   const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} tags={[]} />);
 
   expect(component.find('img').prop('alt')).toEqual(expectedAlt);
   expect(component.find('img').prop('src')).toEqual(expectedSrc);
+});
+
+it ('should check if (name/cost/days) props renders properly', () => {
+
+  const expectedName = 'expectedName';
+  const expectedCost = '12,345';
+  const expectedDays = 67;
+
+  const component = shallow(<TripSummary name={expectedName} cost={expectedCost} days={expectedDays} tags={[]} />);
+
+  const title = component.find('.title').text();
+  // Warning: Failed prop type: Invalid prop `days` of type `string` supplied to `TripSummary`, expected `number`.
+
+  expect(title).toEqual(expectedName);
+
 
 
 });
