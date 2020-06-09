1b1e639ce3cb67b153378a9ca30c9c9f4aaab2af
Kamil Bogucki <boguckikamil@hotmail.com>
2020-06-09 20:02:10 +0200

    delete unnecessary comments

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,12 +1,11 @@
 export const formatTime = (time) => {
 
-  if (!time || typeof time !== 'number' || time < 0) return null;
+  if (time == null || typeof time !== 'number' || time < 0) return null;
 
 
-  // --- 66000 seconds =
-  let hour = Math.floor(time / 3600); /*66000 / 3600 = 18,333 -> 18:__:__ */
-  let minute = Math.floor((time / 60) % 60); /* 66000 / 60 = 1100, 1100 % 60 = 20 -> 18:20:__  */
-  let second = Math.floor(time % 60); /* 66000 % 60 = 0 --> 18:20:_0 */
+  let hour = Math.floor(time / 3600);
+  let minute = Math.floor((time / 60) % 60);
+  let second = Math.floor(time % 60);
 
   let newTime = (hour < 10 ? '0' + hour : hour) + ':'
     + (minute < 10 ? '0' + minute : minute) + ':'
